250999d870b7010db2fbdfda4fd011a90ae3e003
Noel OConnor <noel.oconnor@gmail.com>
2016-05-14 20:59:19 +1000

    adding diagnostic

--- src/main/java/com/noctest/TransportDataApplication.java
+++ src/main/java/com/noctest/TransportDataApplication.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 public class TransportDataApplication {
 
     public static void main(String[] args) {
+        System.out.println("Starting up");
         SpringApplication.run(TransportDataApplication.class, args);
     }
 }
